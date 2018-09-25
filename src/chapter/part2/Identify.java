f7275f8520f956797ada6037cfceb0925d69ef19
Brian Barnes <brian.jx.b@gmail.com>
2018-09-25 13:41:26 -0500

    Added splash message

--- src/chapter/part2/Identify.java
+++ src/chapter/part2/Identify.java
@@ -4,6 +4,7 @@ package chapter.part2;
 public class Identify {
    
     public static void main(String args[]) {
+        System.out.println("AP Computer Science");
         Lincoln l = new Lincoln();
         l.doStuff();
         TimeConverter t = new TimeConverter();
