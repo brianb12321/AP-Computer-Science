54ad0e6d0981397a17a09ffeccb6fd6db28dfa6f
Brian Barnes <brian.jx.b@gmail.com>
2018-09-26 13:58:50 -0500

    Added random class.

--- src/chapter/part2/Identify.java
+++ src/chapter/part2/Identify.java
@@ -6,5 +6,7 @@ public class Identify {
     public static void main(String args[]) {
         TimeConverter2 t = new TimeConverter2();
         t.doStuff();
+        // RandomNumbers r = new RandomNumbers();
+        // r.doStuff();
     }
 }
